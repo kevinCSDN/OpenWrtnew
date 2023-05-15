cd1c390582e61a26a4a407fdd2a0e2d942db3ae1
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2023-05-15 11:42:02 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -14,9 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
-# cd lede/package/lean
-# rm -rf luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
