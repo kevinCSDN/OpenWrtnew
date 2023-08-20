874e629bfbfecb9f11036096eb59e9b1e39fd949
qqww4540 <32699966+qqww4540@users.noreply.github.com>
2023-08-20 23:01:48 +0800

    Update diy-part1.sh

--- x86/diy-part1.sh
+++ x86/diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
+ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+ echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
